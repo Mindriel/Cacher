7659eb9ce8b822004376e8f67709b70db61a0694
Mindriel <andrzej.rozenfeld.dev@gmail.com>
2016-10-12 17:06:44 +0200

    Added expectations to tests.

--- jsTests/Tests.js
+++ jsTests/Tests.js
@@ -1,33 +1,47 @@
 'use strict';
 
-const Cacher = require( '../index');
-console.log('!! Cacher.keys:', Object.keys(Cacher));
+const expect = require('chai').expect;
+const Cacher = require('../index');
 
-describe('JS Babel-less tests', function() {
+describe('JS :: Babel-less tests', function() {
     it('context-function', function() {
         const testObj = {
-            someFn: function() {}
+            called: 0,
+            someFn: function() { this.called++; }
         };
-        
+
+        var actionCalled = 0;
+        const action = function() { actionCalled++; };
+
         const getOnClick = new Cacher()
             .context(testObj)
             .functor(testObj.someFn)
             .create();
 
-        getOnClick(1)();
+        getOnClick(1, action)();
+
+        expect(testObj.called).to.equal(1);
+        expect(actionCalled).to.equal(0);
     });
 
     it('action-context-function', function() {
         const testObj = {
-            someFn: function() {}
+            called: 0,
+            someFn: function() { this.called++; }
         };
 
+        var actionCalled = 0;
+        const action = function() { actionCalled++; };
+
         const getOnClickAction = new Cacher()
-            .action()
+            .action(1)
             .context(testObj)
             .functor(testObj.someFn)
             .create();
 
-        getOnClickAction(1)();
+        getOnClickAction(1, action)();
+
+        expect(testObj.called).to.equal(1);
+        expect(actionCalled).to.equal(1);
     });
 });
--- tests/Tests.ts
+++ tests/Tests.ts
@@ -1,25 +1,37 @@
 /// <reference path="../typings/index.d.ts" />
 
+import { expect } from 'chai';
 import Cacher from '../src/Cacher';
 
 describe('Babel-less tests', function() {
     it('context-function', function() {
         const testObj = {
-            someFn: function() {}
+            called: 0,
+            someFn: function() { this.called++; }
         };
+
+        let actionCalled = 0;
+        const action = function() { actionCalled++; };
 
         const getOnClick = new Cacher()
             .context(testObj)
             .functor(testObj.someFn)
             .create();
 
-        getOnClick(1)();
+        getOnClick(1, action)();
+
+        expect(testObj.called).to.equal(1);
+        expect(actionCalled).to.equal(0);
     });
 
     it('action-context-function', function() {
         const testObj = {
-            someFn: function() {}
+            called: 0,
+            someFn: function() { this.called++; }
         };
+
+        let actionCalled = 0;
+        const action = function() { actionCalled++; };
 
         const getOnClickAction = new Cacher()
             .action(1)
@@ -27,6 +39,9 @@ describe('Babel-less tests', function() {
             .functor(testObj.someFn)
             .create();
 
-        getOnClickAction(1, ()=>{})();
+        getOnClickAction(1, action)();
+
+        expect(testObj.called).to.equal(1);
+        expect(actionCalled).to.equal(1);
     });
 });
